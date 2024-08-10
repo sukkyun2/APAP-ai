958992dffae13e67aa979c1041583095048e8739
sukkyun2 <goobghd@gmail.com>
2024-08-10 16:02:11 +0900

    feat: 여러 카메라 스트리밍 적용

--- app/api_response.py
+++ app/api_response.py
@@ -24,3 +24,24 @@ class ApiResponse(BaseModel, Generic[T]):
 
     class Config:
         arbitrary_types_allowed = True
+
+
+class ApiListResponse(BaseModel, Generic[T]):
+    code: int = 200
+    message: str = "OK"
+    items: List[T] = None
+
+    @staticmethod
+    def ok():
+        return ApiListResponse(code=200, message="OK")
+
+    @staticmethod
+    def ok_with_data(data: List[T]):
+        return ApiListResponse(code=200, message="OK", items=data)
+
+    @staticmethod
+    def bad_request(error: str):
+        return ApiListResponse(code=400, message=error)
+
+    class Config:
+        arbitrary_types_allowed = True
