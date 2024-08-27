5ca139619782230d44b173786fad83ce053f54f7
sukkyun2 <goobghd@gmail.com>
2024-08-27 22:55:40 +0900

    feat: custom model 추가
    - 여러 카메라를 동시에 송출시 연결끊기는 이슈 수정

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     history_api: str = Field(alias='HISTORY_API_HOST', default='http://localhost:8080')
     yolo_weight_path: str = Field(alias='YOLO_WEIGHT_PATH', default='yolov5su.pt')
+    custom_yolo_weight_path: str = Field(alias='CUSTOM_YOLO_WEIGHT_PATH', default='yolov5su.pt')
     allow_origins: str = Field(alias='ALLOW_ORIGINS', default='')  # TODO Convert to List
 
     class Config:
--- model/operations.py
+++ model/operations.py
@@ -1,12 +1,13 @@
 from enum import StrEnum
 
-from model.detect import estimate_distance, area_intrusion
+from model.detect import estimate_distance, area_intrusion, detect_by_custom_model
 from model.schema import DetectionResult
 
 
 class OperationType(StrEnum):
     ESTIMATE_DISTANCE = "estimate_distance",
     AREA_INTRUSION = "area_intrusion"
+    CUSTOM_MODEL = "custom_model"
 
 
 def define_operation(op: OperationType):
@@ -14,6 +15,8 @@ def define_operation(op: OperationType):
         return handle_estimate_distance
     elif op == OperationType.AREA_INTRUSION:
         return handle_area_intrusion
+    elif op == OperationType.CUSTOM_MODEL:
+        return handle_detect_by_custom_model
 
 
 def handle_estimate_distance(img) -> tuple[bool, DetectionResult]:
@@ -26,3 +29,8 @@ def handle_estimate_distance(img) -> tuple[bool, DetectionResult]:
 def handle_area_intrusion(img) -> tuple[bool, DetectionResult]:
     intrusion, result = area_intrusion(img)
     return intrusion, result
+
+
+def handle_detect_by_custom_model(img) -> tuple[bool, DetectionResult]:
+    result = detect_by_custom_model(img)
+    return len(result.detections) > 0, result
